ead853a2a104f16df788eba85b76573cc700b2ec
Etika Joshi <165855033+etika30@users.noreply.github.com>
2025-08-09 20:22:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+/* *********************** toggle icon navbar ********************** */
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+/* *********************** scroll section active link ********************** */
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(link => link.classList.remove('active'));
+            const activeLink = document.querySelector('header nav a[href*="' + id + '"]');
+            if(activeLink) {
+                activeLink.classList.add('active');
+            }
+        }
+    });
+
+    // sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar on scroll
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+
+/* *********************** scroll reveal ********************** */
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 100,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.skills-container, .projects-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+/* *********************** typed JS ********************** */
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Coder', 'UI/UX Designer', 'Fullstack developer', 'Product Manager'], 
+    typeSpeed: 70,
+    backspeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
+
+
+document.querySelectorAll('.flip-card').forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+  });
+});
